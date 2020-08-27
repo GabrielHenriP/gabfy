c37231955c0959ef3dd3f172a31766bd8955001f
soadhenri <62116988+soadhenri@users.noreply.github.com>
2020-08-27 20:58:31 -0300

    add grab function

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useDrag, useDrop } from 'react-dnd'
 
 import { Container, Label } from './styles'
  
 
-export default function Card({ data }){
+export default function Card({ data, index }){
+    const ref = useRef();
+   
+    const [{ isDragging }, dragRef] = useDrag({
+        item: { type: 'CARD',index , id: data.id, content:data.content },
+        collect: monitor => ({
+            isDragging: monitor.isDragging(),
+        })
+    });
+
+    const [, dropRef] = useDrop({
+        accept: 'CARD',
+        hover(item,monitor){
+            const draggedIndex = item.index;
+            const targetIndex = index;
+
+            if(draggedIndex === targetIndex){
+                return;
+            }
+
+            const targetSize = ref.current.getBoundingClientRect();
+            const targetCenter = (targetSize.bottom - targetSize.top) / 2;
+
+            const draggedOffSet = monitor.getClientOffset();
+            const draggedTop = draggedOffSet.y - targetSize.top
+        }
+    });
+
+    dragRef(dropRef(ref));
+
     return(
-        <Container>
+        <Container ref={ref} isDragging={isDragging} >
             <header>
                 {data.labels.map(label=> <Label key={label} color={label} />)}
             </header>
             <p> { data.content } </p>
-            { data.user && <img src={data.user} /> }
+            { data.user && <img src={data.user} alt="foto"/> }
         </Container>
     );
 }
